25f9dfd460fd33fffa966e3659130fb156a90d33
trung32kj <trung01694011@gmail.com>
2026-04-24 14:04:46 +0700

    email

--- Baitap/email_config.py
+++ Baitap/email_config.py
@@ -6,17 +6,10 @@
 # 5. Copy mật khẩu 16 ký tự và dán vào sender_password
 # Thay đổi thông tin email của bạn ở đây
 
-# EMAIL_CONFIG = {
-#     "smtp_server": "smtp.gmail.com",
-#     "smtp_port": 587,
-#     "sender_email": "your_email@gmail.com",  # Thay bằng email của bạn
-#     "sender_password": "your_app_password",  # Thay bằng App Password của bạn
-#     "receiver_email": "recipient@gmail.com"  # Thay bằng email người nhận
-# }
 EMAIL_CONFIG = {
     "smtp_server": "smtp.gmail.com",
     "smtp_port": 587,
-    "sender_email": "trung01694011@gmail.com",  # Thay bằng email của bạn
-    "sender_password": "schx hzeq pqwr dnyq",  # Thay bằng App Password của bạn
-    "receiver_email": "trung_2351220236@dau.edu.vn"  # Thay bằng email người nhận
+    "sender_email": "your_email@gmail.com",  # Thay bằng email của bạn
+    "sender_password": "your_app_password",  # Thay bằng App Password của bạn
+    "receiver_email": "recipient@gmail.com"  # Thay bằng email người nhận
 }
